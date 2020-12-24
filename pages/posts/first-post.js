9205c636a0aa52a70fb586ee0d2135f5e7fa4e24
soutaschool <sasakisouta110@icloud.com>
2020-12-24 23:00:52 +0900

    getstaticpropsの最初から

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import Link from "next/link";
-import Head from "next/head";
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
-const FirstPost = () => {
-    return(
-        <React.Fragment>
-            <Head>
-                <title>First post</title>
-            </Head>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </React.Fragment>
-    )
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
 }
-
-export default FirstPost;
