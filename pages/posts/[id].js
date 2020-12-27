9c60077d5defe04b8fa2e4a7bc209924ca667d4e
soutaschool <sasakisouta110@icloud.com>
2020-12-27 17:51:39 +0900

    日付の設定

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,33 +1,39 @@
 import Layout from "../../components/layout";
 import {getAllPostIds, getPostData} from "../../lib/posts";
+import {Head} from "next/document";
+import Date from "../../components/date";
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: true
-  }
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: true
+    }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
+export async function getStaticProps({params}) {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
     }
-  }
 }
 
 
 const Post = ({postData}) => {
-  return (
-      <Layout>
-        {postData.title} <br/>
-        {postData.id} <br/>
-        {postData.date} <br/>
-        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
-      </Layout>
-  )
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <Date dateString={postData.date}/>
+            {postData.title} <br/>
+            {postData.id} <br/>
+            {postData.date} <br/>
+            <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+        </Layout>
+    )
 };
 
 export default Post;
